dd6613f0ea021f1f3a6f4e2299f3b402b4d8dc4e
Antoine Popeler <antoine.popeler@student.uclouvain.be>
2016-05-03 09:32:59 +0200

    edited Browser class and added some images

---
+++ app/src/main/java/groupek/lsinf1225_projet/Browser.java
@@ -0,0 +1,13 @@
+package groupek.lsinf1225_projet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Browser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browser);
+    }
+}
