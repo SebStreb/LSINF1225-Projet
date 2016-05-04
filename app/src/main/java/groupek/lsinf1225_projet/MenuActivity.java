47728ff5c627eb95ea7105f61aecaa84ac1ee175
Sébastien Strebelle <sebastien.strebelle@student.uclouvain.be>
2016-05-04 15:32:39 +0200

    Liens dans le menu

--- app/src/main/java/groupek/lsinf1225_projet/MenuActivity.java
+++ app/src/main/java/groupek/lsinf1225_projet/MenuActivity.java
@@ -1,7 +1,9 @@
 package groupek.lsinf1225_projet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.*;
 
 public class MenuActivity extends AppCompatActivity {
@@ -25,5 +27,36 @@ public class MenuActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, android.R.id.text1, menu);
         ListView list = (ListView) findViewById(R.id.listView);
         list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Intent newActivity;
+                switch(position){
+                    case 0:  newActivity = new Intent(MenuActivity.this, ProfilActivity.class);
+                        startActivity(newActivity);
+                        break;
+                    case 1:  newActivity = new Intent(MenuActivity.this, LoginActivity.class); //TODO: Add preferences
+                        startActivity(newActivity);
+                        break;
+                    case 2:  newActivity = new Intent(MenuActivity.this, LoginActivity.class); //TODO: Add broswer
+                        startActivity(newActivity);
+                        break;
+                    case 3:  newActivity = new Intent(MenuActivity.this, Contact.class);
+                        startActivity(newActivity);
+                        break;
+                    case 4:  newActivity = new Intent(MenuActivity.this, LoginActivity.class); //TODO: Add demandes
+                        startActivity(newActivity);
+                        break;
+                    case 5:  newActivity = new Intent(MenuActivity.this, MessagerieActivity.class);
+                        startActivity(newActivity);
+                        break;
+                    case 6:  newActivity = new Intent(MenuActivity.this, MeetActivity.class);
+                        startActivity(newActivity);
+                        break;
+                }
+
+            };
+        });
     }
 }
