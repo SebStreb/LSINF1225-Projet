54c6bc5e8e1f4bd0478546eec736080e4931d937
reirep <pierre.ortegat@student.uclouvain.be>
2016-05-05 14:20:29 +0200

    modfied MeetActivity

--- app/src/main/java/groupek/lsinf1225_projet/MeetActivity.java
+++ app/src/main/java/groupek/lsinf1225_projet/MeetActivity.java
@@ -1,50 +1,64 @@
 package groupek.lsinf1225_projet;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.roomorama.caldroid.CaldroidFragment;
 import com.roomorama.caldroid.CaldroidListener;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Hashtable;
 
 @SuppressLint("SimpleDateFormat")
 public class MeetActivity extends AppCompatActivity {
-    private boolean undo = false;
-    private CaldroidFragment caldroidFragment;
-    private CaldroidFragment dialogCaldroidFragment;
-
-
-    //the hashtables used to know who chose what when
-    private Hashtable<Long, Boolean> daysUser = new Hashtable<>();
-    private Hashtable<Long, Boolean> daysOther = new Hashtable<>();
-    //TODO load here all the dates from the other one
-    //TODO load here all the dates from the current user
-    //TODO load the date sof the two user into the callendar
-
     //to color the day
     /*
     *   BlueUser: #4169E1 - Royal Blue - The color used when the user select a day
     *   BlueOther: #87CEEB - Sky Blue - The color used to show the user whitch dates have been picked by the other user
     *   Red: #B22222 - Fire Brick - The color used when they both agree on the same date
     */
-    final ColorDrawable green = new ColorDrawable(Color.GREEN);
-    final ColorDrawable blue = new ColorDrawable(Color.parseColor("#4169E1"));
-    final ColorDrawable red = new ColorDrawable(Color.RED);
+    final ColorDrawable blueOther = new ColorDrawable(Color.parseColor("#87CEEB"));
+    final ColorDrawable blueUser = new ColorDrawable(Color.parseColor("#4169E1"));
+    final ColorDrawable red = new ColorDrawable(Color.parseColor("#B22222"));
+    //internal var to handle the parameters and get the correct dates from the db
+    int idFrom, idTo;
+    Context context;
+    //TODO load here all the dates from the other one
+    //TODO load here all the dates from the current user
+    //TODO load the date sof the two user into the callendar
+    private boolean undo = false;
+    private CaldroidFragment caldroidFragment;
+    private CaldroidFragment dialogCaldroidFragment;
+    //the hashtables used to know who chose what when
+    private Hashtable<Long, Boolean> daysUser;
+    private Hashtable<Long, Boolean> daysOther;
+
+    //this is a default handler, it only purpose is to handle the bad request
+    public MeetActivity() {
+        Log.wtf("Meetacticty", "Bad request to MeetActuvity, 0 args");
+    }
+
+    //this is a default handler, it only purpose is to handle the bad request
+    public MeetActivity(int idFrom) {
+        this.idFrom = idFrom;
+        Log.wtf("Meetacticty", "Bad request to MeetActuvity, 1 arg");
+    }
+
+    public MeetActivity(int idFrom, int idTo, Context context) {
+        this.context = context;
+    }
+
 
     private void setCustomResourceForDates() {
         Calendar cal = Calendar.getInstance();
@@ -54,13 +68,6 @@ public class MeetActivity extends AppCompatActivity {
 
         // Max date is next 7 days
         cal.add(Calendar.DATE, 7);
-
-        /*
-        if (caldroidFragment != null) {
-            ColorDrawable blue = new ColorDrawable(getResources().getColor(R.color.blue));
-            ColorDrawable green = new ColorDrawable(Color.GREEN);
-        }
-        */
     }
 
     @Override
@@ -71,7 +78,6 @@ public class MeetActivity extends AppCompatActivity {
         final SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy");
 
         // Setup caldroid fragment
-        // **** If you want normal CaldroidFragment, use below line ****
         caldroidFragment = new CaldroidFragment();
 
 
@@ -82,7 +88,8 @@ public class MeetActivity extends AppCompatActivity {
         caldroidFragment.setMinDate(calM.getTime());
 
         // If Activity is created after rotation
-        if (savedInstanceState != null) {caldroidFragment.restoreStatesFromKey(savedInstanceState, "CALDROID_SAVED_STATE");
+        if (savedInstanceState != null) {
+            caldroidFragment.restoreStatesFromKey(savedInstanceState, "CALDROID_SAVED_STATE");
         }
         // If activity is created from fresh
         else {
@@ -106,6 +113,30 @@ public class MeetActivity extends AppCompatActivity {
         t.replace(R.id.calendar1, caldroidFragment);
         t.commit();
 
+
+        this.idFrom = (int) savedInstanceState.get("myID");
+        this.idTo = (int) savedInstanceState.get("thisID");
+
+
+        //initialising the hashtable and the colors of the calendar
+        daysUser = new Hashtable<>();
+        daysOther = new Hashtable<>();
+        Date userD[] = searchDatabase(this.context);
+        for (Date d : userD) {
+            daysUser.put(d.getTime(), true);
+            caldroidFragment.setBackgroundDrawableForDate(blueUser, new Date(d.getTime()));
+        }
+        Date userO[] = searchDatabase(this.context);
+        for (Date d : userO) {
+            daysOther.put(d.getTime(), true);
+            if (daysUser.get(t) != null) {
+                caldroidFragment.setBackgroundDrawableForDate(red, new Date(d.getTime()));
+            } else {
+                caldroidFragment.setBackgroundDrawableForDate(blueOther, new Date(d.getTime()));
+            }
+        }
+        caldroidFragment.refreshView();
+
         // Setup listener
         final CaldroidListener listener = new CaldroidListener() {
 
@@ -114,19 +145,29 @@ public class MeetActivity extends AppCompatActivity {
             */
             @Override
             public void onSelectDate(Date date, View view) {
-                if(daysUser.get(date.getTime()) != null){
-                    if(daysUser.get(date.getTime())==true){
-                        caldroidFragment.clearBackgroundDrawableForDate(date);
+                if (daysUser.get(date.getTime()) != null) {//the user has already click on this date
+                    if (daysUser.get(date.getTime()) == true) {
+                        if (daysOther.get(date.getTime()) != null && daysOther.get(date.getTime()) == true) {
+                            caldroidFragment.setBackgroundDrawableForDate(blueOther, date);
+                        } else {
+                            caldroidFragment.clearBackgroundDrawableForDate(date);
+                        }
                         daysUser.put(date.getTime(), false);
+                    } else {
+                        if (daysOther.get(date.getTime()) != null && daysOther.get(date.getTime()) == true) {
+                            caldroidFragment.setBackgroundDrawableForDate(blueOther, date);
+                        } else {
+                            caldroidFragment.setBackgroundDrawableForDate(blueUser, date);
+                        }
+                        daysUser.put(date.getTime(), true);
                     }
-                    else {
-                        caldroidFragment.setBackgroundDrawableForDate( blue, date);
-                        daysUser.put(date.getTime(), false);
-                    }
-                }
-                else {
+                } else {//the user has never click on this date
                     daysUser.put(date.getTime(), true);
-                    caldroidFragment.setBackgroundDrawableForDate( blue, date);
+                    if (daysOther.get(date.getTime()) != null && daysOther.get(date.getTime()) == true) {
+                        caldroidFragment.setBackgroundDrawableForDate(blueOther, date);
+                    } else {
+                        caldroidFragment.setBackgroundDrawableForDate(blueUser, date);
+                    }
                 }
                 caldroidFragment.refreshView();
             }
