f658e65d7c655fd6f23c821d494024715155327a
Sébastien Strebelle <sebastien.strebelle@student.uclouvain.be>
2016-05-05 17:53:59 +0200

    je sais plus

--- app/src/main/java/groupek/lsinf1225_projet/MessagerieActivity.java
+++ app/src/main/java/groupek/lsinf1225_projet/MessagerieActivity.java
@@ -15,17 +15,14 @@ import java.util.ArrayList;
 
 public class MessagerieActivity extends AppCompatActivity {
 
-    private Context con;
-    private int ID;
+    private User me;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messagerie);
         Bundle b = getIntent().getExtras();
-        con = this;
-        ID = b.getInt("id");
-        User me = new User(this, ID);
+        me = new User(this, b.getInt("id"));
         String[] amis = me.listeAmis();
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, amis);
@@ -38,11 +35,12 @@ public class MessagerieActivity extends AppCompatActivity {
                 String amiName = (String) list.getItemAtPosition(position);
                 String prenom = amiName.split(" ")[0];
                 String nom = amiName.split(" ")[1];
+                User other = new User(me.getContext(), prenom, nom);
                 Intent intent = new Intent(MessagerieActivity.this, ChatActivity.class);
                 Bundle b = new Bundle();
                 b.putString("nom", prenom);
-                b.putInt("myID", ID); //Mettre mon ID
-                b.putInt("hisID", User.find(prenom, nom, con));
+                b.putInt("myID", me.getId());
+                b.putInt("hisID", other.getId());
                 intent.putExtras(b);
                 startActivity(intent);
             }
