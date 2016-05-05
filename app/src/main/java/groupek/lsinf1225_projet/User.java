8d10e126f485e00cc0b35b34c34fb2f8ce2dd2c3
Sébastien Strebelle <sebastien.strebelle@student.uclouvain.be>
2016-05-05 11:56:30 +0200

    User Ok ?

---
+++ app/src/main/java/groupek/lsinf1225_projet/User.java
@@ -0,0 +1,98 @@
+package groupek.lsinf1225_projet;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.*;
+
+public class User {
+
+    private Context context;
+    private int id;
+    private Hashtable<String,Object> caracteristiques;
+    private static final String[] carac = {"Nom","Prenom","Genre","Age","Cheveux","Yeux","Rue","Code Postal","Localite","Pays","Telephone","Inclinaison","Facebook","Langue(s)"};
+
+
+    public User(Context con, int id){
+        this.id = id;
+        this.context = con;
+        caracteristiques = new Hashtable<String,Object>();
+        String[] data = searchDatabase();
+        for (int i = 0; i < carac.length; i++)
+            setOpt(carac[i], data[i]);
+    }
+
+    public Object getOpt(String field) {
+        if(this.caracteristiques.containsKey(field))
+            return caracteristiques.get(field);
+        return null;
+    }
+
+    /*
+    * @pre : field and value must NOT be null
+    * @post : set value associated to field in caracteristiques
+     */
+    public void setOpt(String field, Object value) {
+        if(this.caracteristiques.containsKey(field))
+            caracteristiques.put(field,value);
+    }
+
+    public void save() {
+        DatabaseHelper myHelper = new DatabaseHelper(context);
+        SQLiteDatabase database =  myHelper.open();
+        String query = "REPLACE INTO user(Nom, Prenom, Genre, Age, Cheveux, Yeux, Rue, Code Postal, Localite, Pays, Telephone, Inclinaison, Facebook, Langue) VALUES("
+                + "'"+getOpt("Nom")+"',"
+                + "'"+getOpt("Prenom")+"',"
+                + "'"+getOpt("Genre")+"',"
+                + "'"+getOpt("Age")+"',"
+                + "'"+getOpt("Cheveux")+"',"
+                + "'"+getOpt("Yeux")+"',"
+                + "'"+getOpt("Rue")+"',"
+                + "'"+getOpt("Code Postal")+"',"
+                + "'"+getOpt("Localite")+"',"
+                + "'"+getOpt("Pays")+"',"
+                + "'"+getOpt("Telephone")+"',"
+                + "'"+getOpt("Inclinaison")+"',"
+                + "'"+getOpt("Facebook")+"',"
+                + "'"+getOpt("Langue")+"')";
+        database.execSQL("query");
+    }
+
+    private String[] searchDatabase() {
+        DatabaseHelper myHelper = new DatabaseHelper(context);
+        SQLiteDatabase database =  myHelper.open();
+        String [] param = new String [1];
+        param[0] = String.valueOf(id);
+        Cursor cursor = database.rawQuery("SELECT * FROM user WHERE ID = ? ", param);
+        String [] donnees = new String [21];
+        for(int i=0; i< donnees.length; i++){
+            donnees[i]=cursor.getString(i);
+        }
+        cursor.close();
+        return donnees;
+    }
+
+    public String[] listeAmis() {
+        DatabaseHelper myHelper = new DatabaseHelper(context);
+        SQLiteDatabase database =  myHelper.open();
+        String[] param = {Integer.toString(id)};
+        String query = "SELECT DISTINCT U.ID FROM user U, relations R WHERE (U.ID = ID_to and R.ID_from = ? and R.EtatReq = ?) or ( U.ID = R.ID_from and R.ID_to = ? and R.EtatReq = ?);";
+        Cursor cursor = database.rawQuery(query, param);
+        ArrayList<String> list = new ArrayList<String>();
+        while (!cursor.isAfterLast()) {
+            int IdAmi = cursor.getInt(0);
+            String[] param2 = {Integer.toString(IdAmi)};
+            Cursor cursor2 = database.rawQuery("SELECT Prenom, Nom FROM user WHERE ID = ?;", param2);
+            String prenom = cursor2.getString(0);
+            String nom = cursor2.getString(1);
+            cursor2.close();
+            String nomPrenom = prenom+" "+nom;
+            list.add(nomPrenom);
+            cursor.moveToNext();
+        }
+        return list.toArray(new String[list.size()]);
+    }
+
+
+}
